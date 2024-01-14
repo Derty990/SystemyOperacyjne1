ad81d6e35aecc7997a8e9c641c9b3943f909f3b5
Bartek Kluczewski <kluczbart@gmail.com>
2024-01-14 09:51:01 +0100

    prosty kalkulator python

---
+++ calculator.py
@@ -0,0 +1,22 @@
+
+
+def add(x, y)
+return x+y
+
+def substract(x,y)
+return x-y
+
+def multiply(x,y)
+return x-y
+
+def divide(x,y)
+if y!=0
+return x/y
+else return "W mianowniku jest 0! nie możesz tego podzielic."
+
+result = add(200, 3)
+print ("200 + 3 = ", result)
+result = divide(20, 6)
+print ("20 / 6 = ", result)
+result = substract(20,3)
+print ("20 - 3 = ", result)
